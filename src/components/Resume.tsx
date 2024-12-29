c8ff2a1ad90b70f9322729b0d8df84e9aa59bd42
André Company Gianesi <andrecompanygianesi@Andres-MacBook-Air.local>
2024-12-29 01:16:58 -0500

    add Resume

---
+++ src/components/Resume.tsx
@@ -0,0 +1,19 @@
+const Resume = () => {
+    return (
+      <section id="resume">
+        <h2>Resume</h2>
+        <a href="/path/to/resume123.pdf" download>Download My Resume</a>
+        
+        <h3>Skills and Proficiencies</h3>
+        <ul>
+          <li>JavaScript</li>
+          <li>React.js</li>
+          <li>HTML/CSS</li>
+          <li>Node.js</li>
+          <li>Cooming Soon</li>
+        </ul>
+      </section>
+    );
+  };
+  
+  export default Resume;
